782f5e2389603d95456a8f9836bb9bb40249e69e
Gavin M <37052167+g-monroe@users.noreply.github.com>
2018-08-26 17:22:55 -0500

    Update search.php

--- youStream-master/search.php
+++ youStream-master/search.php
@@ -7,7 +7,7 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
      * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
      * Please ensure that you have enabled the YouTube Data API for your project.
      */
-    $DEVELOPER_KEY = 'AIzaSyBiPPSB0tkOTrrB7X1X4TH5GVbqGVR8U60';
+    $DEVELOPER_KEY = 'KEY';
 
     $client = new Google_Client();
     $headers = array('Referer' => "http://gmonroe.org");
